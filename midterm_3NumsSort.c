9aa3a4626389cb5cbe7c8397527a99dbb356a57b
pathorn <164243298+pathornn@users.noreply.github.com>
2024-09-01 01:55:04 +0700

    Create midterm_3NumsSort.c

---
+++ midterm_3NumsSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //input
+    int a, b, c, temp;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a >= b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if(a >= c){
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if(b >= c){
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
