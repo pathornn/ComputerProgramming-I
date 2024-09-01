225fb9cff0f2eefa74cd52f391225740a89a09fd
pathorn <164243298+pathornn@users.noreply.github.com>
2024-09-01 14:38:35 +0700

    Create midterm_rectangle_area.c

---
+++ midterm_rectangle_area.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double w, h;
+    scanf("%lf %lf", &w, &h);
+
+    if(w <= 0 && h <= 0){
+        printf("invalid width and height");
+    } else if(w <= 0){
+        printf("invalid width");
+    } else if(h <= 0){
+        printf("invalid height");
+    } else{
+        printf("%.2lf", w*h);
+    }
+
+    return 0;
+}
