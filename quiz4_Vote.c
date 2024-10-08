697852608dcda5cbf938a7e1ba1ca57812a5bb32
pathorn <164243298+pathornn@users.noreply.github.com>
2024-10-08 23:06:24 +0700

    Update quiz4_Vote.c

--- quiz4_Vote.c
+++ quiz4_Vote.c
@@ -18,10 +18,8 @@ int main()
 
     for(int i=1; i<=K; i++){
         for(int ii=1; ii<=people[i]; ii++){
-            //printf("people = %d, choose = %d\n", i, people[i]);
             if(ii == people[i]){
                 candidate[ii] ++;
-                //printf("candidate = %d\n\n", candidate[i]);
             }
         }
     }
@@ -31,7 +29,6 @@ int main()
         if(candidate[i] > highestScore){
             mostVoteCandidate = i; highestScore = candidate[i];
         }
-        //printf("candidate[%d] = %d\n", i, candidate[i]);
     }
 
     printf("%d\n%d", mostVoteCandidate, highestScore);
